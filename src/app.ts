371c9eee9bd7c2ebf35f6807a306f260ec42d33a
avivharuzi <avivharuzi@gmail.com>
2018-12-14 08:38:01 +0200

    Update app.ts and remove tests folder for now

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ import * as bodyParser from 'body-parser';
 
 class App {
   public express: express.Application;
-  
+
   public constructor() {
     this.express = express();
     this.middleware();
   }
-  
+
   private middleware() {
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
