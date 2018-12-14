6f65076b316f76d39a1a88288d1d1315cd68cf31
avivharuzi <avivharuzi@gmail.com>
2018-12-14 17:59:26 +0200

    Remove err from server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,6 @@ import app from './app';
 const HOSTNAME: string = process.env.SERVER_HOSTNAME || '127.0.0.1';
 const PORT: number = !process.env.SERVER_PORT ? 3000 : parseInt(process.env.SERVER_PORT);
 
-app.listen(PORT, HOSTNAME, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Server running at http://${HOSTNAME}:${PORT}`);
-  }
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}`);
 });
