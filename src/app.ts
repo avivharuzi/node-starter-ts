b0cf53ee7f76939d1f147742c248ea36c9ee499c
avivharuzi <avivharuzi@gmail.com>
2018-12-14 08:52:54 +0200

    Add cors package

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
+import * as cors from 'cors';
 
 class App {
   public express: express.Application;
@@ -13,6 +14,7 @@ class App {
 
   private middleware() {
     this.express.use(compression({ threshold: 0 }));
+    this.express.use(cors());
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
     this.express.use(cookieParser());
