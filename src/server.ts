30e53147e378c2c966095c02aa5a1c33356eb5d9
Aviv Haruzi <avivharuzi@gmail.com>
2020-05-09 13:03:49 +0300

    Clean up files

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const HOSTNAME: string = process.env.SERVER_HOSTNAME || '127.0.0.1';
-const PORT: number = !process.env.SERVER_PORT ? 3000 : parseInt(process.env.SERVER_PORT);
-
-app.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${HOSTNAME}:${PORT}`);
-});
