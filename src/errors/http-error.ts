d03712fdf52ab75832a377916e8ca52b3b3a5a4a
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-02 20:42:49 +0300

    Fix HttpError class

--- src/errors/http-error.ts
+++ src/errors/http-error.ts
@@ -2,7 +2,7 @@ export class HttpError extends Error {
   httpCode: number;
   errors: any[];
 
-  constructor(httpCode: number, message: string, errors = []) {
+  constructor(httpCode: number, message: string, errors: any[] = []) {
     super(message);
     this.httpCode = httpCode;
     this.errors = errors;
